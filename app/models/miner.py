a8b10527557b83655bcd11697a37d9063f7583b2
Sergio Clemente <sergioclemente@gmail.com>
2018-01-05 14:41:48 -0800

    * Rename remote_host_port to http_admin_host_port * Convert units from 19000 MH/s to 19 GH/s for example

--- app/models/miner.py
+++ app/models/miner.py
@@ -4,10 +4,10 @@ from app import db
 class Miner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     local_ip = db.Column(db.String(15), unique=True, nullable=False)
-    remote_hostport = db.Column(db.String(255), unique=True, nullable=False)
+    http_admin_host_port = db.Column(db.String(255), unique=True, nullable=False)
     model_id = db.Column(db.Integer, db.ForeignKey('miner_model.id'), nullable=False)
     model = db.relationship("MinerModel", backref="miners")
     remarks = db.Column(db.String(255), nullable=True)
 
     def __repr__(self):
-        return "Miner(local_ip='{}', remote_hostport='{}', model='{}', remarks='{}')".format(self.local_ip, self.remote_hostport, self.model, self.remarks)
+        return "Miner(local_ip='{}', http_admin_host_port='{}', model='{}', remarks='{}')".format(self.local_ip, self.http_admin_host_port, self.model, self.remarks)
--- app/views/antminer.py
+++ app/views/antminer.py
@@ -19,6 +19,19 @@ from datetime import timedelta
 import time
 
 
+# Update from one unit to the next if the value is greater than 1000.
+# e.g. update_unit_and_value(1000, "GH/s") => (1, "TH/s")
+def update_unit_and_value(value, unit):
+    if value > 1000:
+        value = value / 1000
+        if unit == 'MH/s':
+            unit = 'GH/s'
+        elif unit == 'GH/s':
+            unit = 'TH/s'
+        else:
+            assert False, "Unsupported unit: {}".format(unit)
+    return (value, unit)
+
 @app.route('/')
 def miners():
     # Init variables
@@ -34,10 +47,13 @@ def miners():
     hash_rates = {}
     hw_error_rates = {}
     uptimes = {}
-    total_hash_rate_per_model = {"L3+": 0,
-                                 "S7": 0,
-                                 "S9": 0,
-                                 "D3": 0}
+    # TODO(sergioclemente): Abstract these configurations in a separate class. 
+    mapping = {'L3+': 'MH/s', 'S7': 'GH/s', 'S9': 'GH/s', 'D3': 'MH/s'}
+    total_hash_rate_per_model = {"L3+": {"value": 0, "unit": mapping["L3+"]},
+                                 "S7": {"value": 0, "unit": mapping["S7"]},
+                                 "S9": {"value": 0, "unit": mapping["S9"]},
+                                 "D3": {"value": 0, "unit": mapping["D3"]}}
+
     errors = False
     miner_errors = {}
 
@@ -76,7 +92,7 @@ def miners():
                           sorted(miner_stats['STATS'][1].keys(), key=lambda x: str(x)) if
                           re.search("fan" + '[0-9]', fan) if miner_stats['STATS'][1][fan] != 0]
             # Get GH/S 5s
-            ghs5s = miner_stats['STATS'][1]['GHS 5s']
+            ghs5s = float(str(miner_stats['STATS'][1]['GHS 5s']))
             # Get HW Errors
             hw_error_rate = miner_stats['STATS'][1]['Device Hardware%']
             # Get uptime
@@ -89,10 +105,11 @@ def miners():
                                 })
             temperatures.update({miner.local_ip: temps})
             fans.update({miner.local_ip: {"speeds": fan_speeds}})
-            hash_rates.update({miner.local_ip: ghs5s})
+            value, unit = update_unit_and_value(ghs5s, mapping[miner.model.model])
+            hash_rates.update({miner.local_ip: "{:3.2f} {}".format(value, unit)})
             hw_error_rates.update({miner.local_ip: hw_error_rate})
             uptimes.update({miner.local_ip: uptime})
-            total_hash_rate_per_model[miner.model.model] += float(str(ghs5s))
+            total_hash_rate_per_model[miner.model.model]["value"] += ghs5s
             active_miners.append(miner)
 
             # Flash error messages
@@ -132,6 +149,14 @@ def miners():
     # flash("WARNING !!! Check temperatures on your miner", "warning")
     # flash("ERROR !!!Check board(s) on your miner", "error")
 
+    # Convert the total_hash_rate_per_model into a data structure that the template can
+    # consume.
+    total_hash_rate_per_model_temp = {}
+    for key in total_hash_rate_per_model:
+        value, unit = update_unit_and_value(total_hash_rate_per_model[key]["value"], total_hash_rate_per_model[key]["unit"])
+        if value > 0:
+            total_hash_rate_per_model_temp[key] = "{:3.2f} {}".format(value, unit)
+
     end = time.clock()
     loading_time = end - start
     return render_template('myminers.html',
@@ -146,7 +171,7 @@ def miners():
                            hash_rates=hash_rates,
                            hw_error_rates=hw_error_rates,
                            uptimes=uptimes,
-                           total_hash_rate_per_model=total_hash_rate_per_model,
+                           total_hash_rate_per_model=total_hash_rate_per_model_temp,
                            loading_time=loading_time,
                            miner_errors=miner_errors,
                            )
@@ -155,7 +180,7 @@ def miners():
 @app.route('/add', methods=['POST'])
 def add_miner():
     local_ip = request.form['local_ip']
-    remote_hostport = request.form['remote_hostport']
+    http_admin_host_port = request.form['http_admin_host_port']
     miner_model_id = request.form.get('model_id')
     miner_remarks = request.form['remarks']
 
@@ -164,7 +189,7 @@ def add_miner():
     #    return "IP Address already added"
 
     try:
-        miner = Miner(local_ip=local_ip, remote_hostport=remote_hostport, model_id=miner_model_id, remarks=miner_remarks)
+        miner = Miner(local_ip=local_ip, http_admin_host_port=http_admin_host_port, model_id=miner_model_id, remarks=miner_remarks)
         db.session.add(miner)
         db.session.commit()
         flash("Miner with Local IP Address {} added successfully".format(miner.local_ip), "success")
