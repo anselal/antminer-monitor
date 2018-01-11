66e5fe4e5d53d7f0c7644b94186331c9325506a8
sergioclemente <sergioclemente@gmail.com>
2018-01-10 20:22:57 -0800

    Addressing one comment

--- app/views/antminer.py
+++ app/views/antminer.py
@@ -18,11 +18,11 @@ import re
 from datetime import timedelta
 import time
 
-# Update from one unit to the next if the value is greater than 1000.
-# e.g. update_unit_and_value(1000, "GH/s") => (1, "TH/s")
+# Update from one unit to the next if the value is greater than 1024.
+# e.g. update_unit_and_value(1024, "GH/s") => (1, "TH/s")
 def update_unit_and_value(value, unit):
-    if value > 1000:
-        value = value / 1000
+    while value > 1024:
+        value = value / 1024
         if unit == 'MH/s':
             unit = 'GH/s'
         elif unit == 'GH/s':
