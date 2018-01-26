12a50524f398c4fca4d4d45bd9854ee2e2d8ccc3
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-01-26 19:35:43 +0200

    :up: update(create_db): Add support for T9
    Closes #25

--- create_db.py
+++ create_db.py
@@ -8,6 +8,7 @@ models.append(MinerModel(model='L3+', chips='72,72,72,72', temp_keys='temp2_', d
 models.append(MinerModel(model='S7', chips='45,45,45', temp_keys='temp', description='Bitcoin Miner 4.5 TH/s'))
 models.append(MinerModel(model='S9', chips='63,63,63', temp_keys='temp2_', description='Bitcoin Miner 13.5 TH/s'))
 models.append(MinerModel(model='D3', chips='60,60,60', temp_keys='temp2_', description='DASH Miner 17 GH/s'))
+models.append(MinerModel(model='T9', chips='57,57,57', temp_keys='temp2_', description='Bitcoin Miner 12.5 TH/s'))
 settings = []
 settings.append(Settings(name="temperature_alert", value="80", description=""))
 settings.append(Settings(name="email_alert", value="True", description="Whether to send an email on alert"))
@@ -21,7 +22,7 @@ try:
         db.session.add(setting)
         db.session.commit()
 except IntegrityError:
-    print("Database already exists.")
+    print("[INFO] Database already exists.")
 else:
-    print("Database successfully created.")
+    print("[INFO] Database successfully created.")
 
