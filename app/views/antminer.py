44fa1d2b7e017f7ff96ea2cd3fc32bec33c82030
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-01-28 02:10:41 +0200

    :bug: fix(antminer): Fix add support for T9, A3, L3
    Didn't updated total_hash_rate_per_model

--- app/views/antminer.py
+++ app/views/antminer.py
@@ -55,7 +55,11 @@ def miners():
     total_hash_rate_per_model = {"L3+": {"value": 0, "unit": "MH/s" },
                                 "S7": {"value": 0, "unit": "GH/s" },
                                 "S9": {"value": 0, "unit": "GH/s" },
-                                "D3": {"value": 0, "unit": "MH/s" }}
+                                "D3": {"value": 0, "unit": "MH/s" },
+                                "T9": {"value": 0, "unit": "TH/s" },
+                                "A3": {"value": 0, "unit": "GH/s" },
+                                "L3": {"value": 0, "unit": "MH/s" },}
+                                
     errors = False
     miner_errors = {}
 
