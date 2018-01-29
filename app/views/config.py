3f541fd8586694bd2fe6681c4159fc5491a40b3c
sergioclemente <sergioclemente@gmail.com>
2018-01-28 21:17:15 -0800

    Adding restart

---
+++ app/views/config.py
@@ -0,0 +1,9 @@
+import os
+
+GMAIL_USER = os.environ["GMAIL_USER"]
+GMAIL_PWD = os.environ["GMAIL_PWD"]
+EMAIL_TO = os.environ["EMAIL_TO"]
+DOMAIN_ADDR = os.environ["DOMAIN_ADDR"]
+
+BASIC_AUTH_USER = os.environ["BASIC_AUTH_USER"]
+BASIC_AUTH_PWD = os.environ["BASIC_AUTH_PWD"]
