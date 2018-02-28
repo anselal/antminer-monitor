c6fcf2d95118f80d0a9c1823a80fa36a5cafe39d
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-02-28 03:28:33 +0200

    :up: update(app): Add runserver command

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 server = Server(host='0.0.0.0', port=5000)
+manager.add_command('runserver', server)
 
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
