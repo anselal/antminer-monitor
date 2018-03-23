55c753d6def147447a7a8f4e0e1d5053c69ef30f
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-03-23 02:04:56 +0200

    :shirt: refactor(views): Create antminer_json blueprint

--- antminermonitor/blueprints/asicminer/views/antminer_json.py
+++ antminermonitor/blueprints/asicminer/views/antminer_json.py
@@ -1,24 +1,25 @@
-from flask import jsonify
+from flask import Blueprint, jsonify
 from app import app
 from lib.pycgminer import (get_summary,
                                get_pools,
                                get_stats,
                                )
 
+antminer_json = Blueprint('antminer_json', __name__, template_folder='../templates')
 
-@app.route('/<ip>/summary')
+@antminer_json.route('/<ip>/summary')
 def summary(ip):
     output = get_summary(ip)
     return jsonify(output)
 
 
-@app.route('/<ip>/pools')
+@antminer_json.route('/<ip>/pools')
 def pools(ip):
     output = get_pools(ip)
     return jsonify(output)
 
 
-@app.route('/<ip>/stats')
+@antminer_json.route('/<ip>/stats')
 def stats(ip):
     output = get_stats(ip)
     return jsonify(output)
