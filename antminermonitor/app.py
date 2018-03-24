0b2d9b5ce8a8a9c15fe27d266c02cee919df6dee
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-03-25 01:19:30 +0200

    :shirt: refactor(app): Apply Factory Design Pattern

--- antminermonitor/app.py
+++ antminermonitor/app.py
@@ -1,40 +1,84 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager, Server
-from flask_migrate import Migrate
+
+from antminermonitor.blueprints.asicminer import antminer, antminer_json
+from antminermonitor.extensions import (
+    db,
+    migrate,
+)
+from antminermonitor.blueprints.asicminer.models.miner import Miner
+from antminermonitor.blueprints.asicminer.models.miner_model import MinerModel
+from antminermonitor.blueprints.asicminer.models.settings import Settings
 
 import logging
 import os
 
+
 __version__ = "v0.4.0"
 basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config.settings')
-app.config.from_pyfile('settings.py', silent=True)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
-server = Server(host='0.0.0.0', port=5000)
-manager.add_command('runserver', server)
 
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-app.logger.setLevel(logging.DEBUG)
+def create_app(script_info=None, settings_override=None):
+    """
+    Create a Flask application using the app factory pattern.
 
-# logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
+    :return: Flask app
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
 
-# create a file handler
-handler = logging.FileHandler(os.path.join(basedir, 'logs/antminer_monitor.log'), mode='a')  # mode 'a' is default
-handler.setLevel(logging.WARNING)
+    if settings_override:
+        app.config.update(settings_override)
 
-# create a logging format
-formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
-handler.setFormatter(formatter)
+    app.register_blueprint(antminer)
+    app.register_blueprint(antminer_json)
+    extensions(app)
 
-# add handlers to the logger
-logger.addHandler(handler)
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(app=app, db=db, Miner=Miner, MinerModel=MinerModel,
+                    Settings=Settings)
 
-from app.views import antminer, antminer_json
+    return app
+
+
+def create_logger(app=None):
+    """
+
+    """
+    app = app or create_app()
+    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(app.config['LOG_LEVEL'])
+
+    # logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.WARNING)
+
+    # create a file handler
+    handler = logging.FileHandler(os.path.join(
+        basedir, 'logs/antminer_monitor.log'), mode='a')  # mode 'a' is default
+    handler.setLevel(logging.WARNING)
+
+    # create a logging format
+    formatter = logging.Formatter(
+        '%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+    handler.setFormatter(formatter)
+
+    # add handlers to the logger
+    logger.addHandler(handler)
+
+    return logger
+
+
+def extensions(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+
+    :param app: Flask application instance
+    :return: None
+    """
+    db.init_app(app)
+    migrate.init_app(app)
+
+    return
