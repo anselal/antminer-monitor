5498486a2a967fb1bdcd79a6df3f4c81edba3aff
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-09-11 14:47:45 +0300

    :fix: bug(migrate): Flask migrate does not work due to relative database dir
    Closes #120

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+import os
+
 __VERSION__ = '0.4.0'
 
 DEBUG = True
@@ -5,6 +7,9 @@ LOG_LEVEL = 'DEBUG'  # CRITICAL / ERROR / WARNING / INFO / DEBUG
 
 SECRET_KEY = 'super secret key'
 
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+
 # SQLAlchemy
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db/app.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+    os.path.join(basedir, 'antminermonitor/db/app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
