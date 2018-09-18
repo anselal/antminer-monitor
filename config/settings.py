8bc0e0587131baeaffc816e6b03fb20f38e6b895
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-09-18 09:33:51 +0300

    :wrench: Load .env settings if exist

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+load_dotenv(os.path.join(basedir, '.env'))
 
 __VERSION__ = '0.4.0'
 
 DEBUG = True
 LOG_LEVEL = 'DEBUG'  # CRITICAL / ERROR / WARNING / INFO / DEBUG
 
-SECRET_KEY = 'super secret key'
-
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'super secret key'
 
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
