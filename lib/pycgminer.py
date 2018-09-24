27cd48404ec0ece930f9c22366c28c9d1cf19bac
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2018-09-24 13:37:07 +0300

    :copyright: docs(pycgminer): Append me

--- lib/pycgminer.py
+++ lib/pycgminer.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2013 Thomas Sileo
+# Copyright (c) 2017 Anastasios Selalmazidis
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
