da2308de28a0c84adc3b85bf7084e2da9128d750
Anastasios Selalmazidis <t.selalmasidis@gmail.com>
2021-12-22 16:36:44 +0200

    :up: update(requirements): Update WTForms to v3.0.0

--- antminermonitor/blueprints/user/forms.py
+++ antminermonitor/blueprints/user/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import BooleanField, PasswordField, SelectField, TextField
+from wtforms import BooleanField, PasswordField, SelectField, StringField
 from wtforms.validators import EqualTo, Length
 
 
 class LoginForm(FlaskForm):
-    username = TextField('Username', validators=[Length(min=5, max=64)])
+    username = StringField('Username', validators=[Length(min=5, max=64)])
     password = PasswordField(
         'Password', validators=[
             Length(min=6, max=128),
